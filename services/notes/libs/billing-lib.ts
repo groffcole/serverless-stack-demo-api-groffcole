310e031e4f3e7d5c30bcce42e95cd7dc9b2a0c40
Cole Groff <groffcole@gmail.com>
2021-05-17 07:24:03 -0500

    added some simple unit tests

--- services/notes/libs/billing-lib.ts
+++ services/notes/libs/billing-lib.ts
@@ -1,7 +1,5 @@
-const calculateCost = (storage) => {
+export const calculateCost = (storage) => {
   const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
 
   return rate * storage * 100;
 };
-
-export default calculateCost;
